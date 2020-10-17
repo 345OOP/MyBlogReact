60af3753a6a6850fd6e2839c55a1192f136bacba
unknown <lulugonzaga@gmail.com>
2020-10-16 20:02:18 -0400

    first commit

---
+++ src/build/precache-manifest.b23ed3e7339b4bdf57398be406cc89d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905c84f6cacc3aaecaedec6aa50d08eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21002fd52a2e8731ce29",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21002fd52a2e8731ce29",
+    "url": "/static/js/main.19937b4f.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
